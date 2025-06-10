3ac5f9ee46a6f302a96c7ec910dfdeddc0199ab3
Vinícius Marques <marquesv473@gmail.com>
2025-06-10 15:07:58 -0400

    Corrige identação do parser.h

--- parser.h
+++ parser.h
@@ -5,28 +5,28 @@
 
 class Parser
 {
-	private:
-		Scanner* scanner;
-		Token* lToken;
-		SymbolTable* globalST;
-    	SymbolTable* currentST;
+    private:
+        Scanner* scanner;
+        Token* lToken;
+        SymbolTable* globalST;
+        SymbolTable* currentST;
 
-		void advance();
-		void match(int);
-		void initSymbolTable();
-		
-	public:
-		Parser(string);
-		void run();
+        void advance();
+        void match(int);
+        void initSymbolTable();
+        
+    public:
+        Parser(string);
+        void run();
         void program();
         void mainClass();
-		void classDeclaration();
-		void varDeclaration();
-		void methodDeclaration();
-		void type();
-		void statement();
-		void expr();
-		void exprLinha();
-		void exprList();
+        void classDeclaration();
+        void varDeclaration();
+        void methodDeclaration();
+        void type();
+        void statement();
+        void expr();
+        void exprLinha();
+        void exprList();
         void error(string);
 };
